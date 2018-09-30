442f2a01dfe4a8f8bf47d9fbe88ac2be1879cd99
Ahmed Mostafa ElGamal <ad5044hk@gmail.com>
2018-09-30 09:28:18 +0200

    Fcis_task
    Faculty Cis Second year tasks and requirements

---
+++ swaping_task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{//swaping Task
+		Console.WriteLine("Hello World");
+		int a,b,temp;
+		a=10;b=20;
+	Console.WriteLine("a value is: {0:d2} , b value is :{1:d2} ",a,b);
+		temp=a;
+		a=b;
+		b=temp;
+	Console.WriteLine("a value is: {0:d2} , b value is :{1:d2}  ",a,b);
+	}
+}
