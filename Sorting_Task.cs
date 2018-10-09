8867296e8ac88a56e36e99c6ac79f2a95bef5681
Ahmed Mostafa ElGamal <42376831+4hmedher0@users.noreply.github.com>
2018-10-09 14:39:23 +0200

    Add files via upload
    Sorting array using C#

---
+++ Sorting_Task.cs
@@ -0,0 +1,38 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+{
+        
+int [] m1=new int[] {-1,99,66,0,-99,33,45};
+		Sort(m1);
+
+}
+	public static void print (int [] m1)
+	{
+	// print method
+for(int i=0;i<m1.Length;i++)
+{Console.WriteLine("the index {0} is: {1}",i,m1[i]);}
+	}
+	public static void Sort(int [] m1)
+	{
+	
+	for (int i = 1; i < m1.Length; i++)
+{
+ int temp;
+    for (int j = 0; j <m1.Length; j++)
+    {
+        if (m1[j]>m1[i])
+        {//swap method
+        temp=m1[j];
+        m1[j]=m1[i];
+        m1[i]=temp;
+        }
+       
+    }
+} 
+		print(m1);
+	
+	}
+}
