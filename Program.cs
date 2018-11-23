9c1249e381d4230f4f6bb1ad6f8a0859a5868e52
Ahmed Mostafa ElGamal <42376831+4hmedher0@users.noreply.github.com>
2018-11-23 20:13:14 +0200

    LinkedList  1
    LinkedList

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class node
+    {
+        public object data;
+        public node next;
+        public node(object value)
+        {
+            this.data = value;
+            next = null;
+        }
+        public void print()
+        {
+            Console.WriteLine(data);
+            if (next != null)
+            {
+                next.print();  
+            }
+        }
+        public void append(object data)
+        {
+            if (next==null)
+            {
+                next = new node(data);
+            }
+            else
+            {
+                next.append(data);
+            }
+        }
+
+     
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            node mynode = new node("A");
+            mynode.next = new node("1");
+            mynode.next.next = new node("B");
+            mynode.next.next.next = new node("2");
+          
+          //  Console.WriteLine(mynode.next.next.data); // access 
+            mynode.print();
+            Console.WriteLine("__________________________________________\a");
+            mynode.append("C");
+            mynode.append("3");
+            mynode.print();
+
+         
+
+        }
+     }
+
+
+}
